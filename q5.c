eb08bc878c8b748ff95c94786d79bde6bb169e96
Dhyey250807 <dhyeysoria25@gmail.com>
2025-08-13 20:05:20 +0530

    Add files via upload

---
+++ q5.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void main()
+{
+{
+{
+{
+    int a,b,c;
+    printf("enter 2 numbers for addition");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("%d+%d=%d\n",a,b,c);
+}
+int a,b,c;
+    printf("enter 2 numbers for subtraction");
+    scanf("%d%d",&a,&b);
+    c=a-b;
+    printf("%d-%d=%d\n",a,b,c);
+}
+ int a,b,c;
+    printf("enter 2 numbers for multiplication");
+    scanf("%d%d",&a,&b);
+    c=a*b;
+    printf("%d*%d=%d\n",a,b,c);
+}
+ int a,b,c;
+    printf("enter 2 numbers for division");
+    scanf("%d%d",&a,&b);
+    c=a/b;
+    printf("%d/%d=%d\n",a,b,c);
+}
+
